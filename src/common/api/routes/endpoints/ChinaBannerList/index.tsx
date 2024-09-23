1e36e8a665c89543b1f64e6e47ef1e5dc9a67d4c
sergeykochiev <sergeyivanovo547@gmail.com>
2024-09-23 22:45:35 +0300

    fixed missing endpoint entries

--- src/common/api/routes/endpoints/ChinaBannerList/index.tsx
+++ src/common/api/routes/endpoints/ChinaBannerList/index.tsx
@@ -1,7 +1,7 @@
 import ApiEndpointsMapType from "@/common/types/ApiEndpointsMap"
 const ChinaBannerListEndpoint: ApiEndpointsMapType = {
-    1: "gacha_info/nap/cn_gf01/gacha/list.json",
-    2: "gacha_info/nap/prod_gf_cn/gacha/list.json",
+    1: "gacha_info/hk4e/cn_gf01/gacha/list.json",
+    2: "gacha_info/hkrpg/prod_gf_cn/gacha/list.json",
     3: "gacha_info/nap/prod_gf_cn/gacha/list.json"
 } as const
 export default ChinaBannerListEndpoint
